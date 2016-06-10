7f53d79e71c97a2434cb02a23e110d2bde5c2d75
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-06-10 13:06:21 +0200

    reconnect socket on close

--- src/main/resources/nfms/modules/websocket-bus.js
+++ src/main/resources/nfms/modules/websocket-bus.js
@@ -11,27 +11,50 @@ define([ "message-bus" ], function(bus) {
    }
    uri += "//" + loc.host;
    uri += window.location.pathname + "/websocket-bus-endpoint";
-   console.log(uri);
 
-   var socket = new WebSocket(uri);
-   socket.onmessage = function(event) {
-      var message = JSON.parse(event.data);
-      bus.send(message.type, [ message.payload ]);
+   var socket = null;
+
+   function connect() {
+      socket = new WebSocket(uri);
+      socket.onmessage = function(event) {
+         var message = JSON.parse(event.data);
+         bus.send(message.type, [ message.payload ]);
+      }
+
+      socket.onclose = function(event) {
+         bus.send("websocket-reconnect");
+         
+         setInterval(connect, 5000);
+      }
+      socket.onopen= function(event) {
+         bus.send("websocket-connected");
+      }
    }
+   connect();
 
-   return {
-      send : function(name, msg) {
+   function send(name, msg) {
+      if (socket.readyState == 0) {
+         setTimeout(function() {
+            send(name, msg);
+         }, 100);
+      } else {
          socket.send(JSON.stringify({
             type : name,
             payload : msg
          }));
-      },
-      listen : function(name, callback) {
-         var eventListeners = listeners[name];
-         eventListeners = eventListeners ? eventListeners : [];
-         eventListeners.push(callback);
-         listeners[name] = eventListeners;
-      },
+      }
+   }
+
+   function listen(name, callback) {
+      var eventListeners = listeners[name];
+      eventListeners = eventListeners ? eventListeners : [];
+      eventListeners.push(callback);
+      listeners[name] = eventListeners;
+   }
+
+   return {
+      send : send,
+      listen : listen,
 
    }
 
